5ba2d53c0cb034f3986ad2cf5bc7169d62134ff2
Prof. Juliana <150253185+juliana-vc@users.noreply.github.com>
2023-11-08 12:32:19 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,73 +1,94 @@
-const tbody = document.querySelector("tbody")
+const tbody = document.querySelector("tbody");
 
-const descricao = document.querySelector("#descricao")
-const valor = document.querySelector("#valor")
-const tipo = document.querySelector("#tipo")
-const botaoIncluir = document.querySelector("#incluir")
+const descricao = document.querySelector("#descricao");
+const valor = document.querySelector("#valor");
+const tipo = document.querySelector("#tipo");
+const botaoIncluir = document.querySelector("#incluir");
 
-const entrada = document.querySelector(".entrada")
-const saida = document.querySelector(".saida")
-const total = document.querySelector(".total")
+const entrada = document.querySelector(".entrada");
+const saida = document.querySelector(".saida");
+const total = document.querySelector(".total");
 
-let itens
+let itens;
 
 function adicionar() {
-    if (descricao.value === "" || valor.value === "" || tipo === "") {
-        return alert("Preencha todos os campos")
-    }
+  if (descricao.value === "" || valor.value === "" || tipo === "") {
+    return alert("Preencha todos os campos");
+  }
 
-    itens.push({
-        desc: descricao.value,
-        //converte o número para absoluto
-        val: Math.abs(valor.value),
-        tipo: tipo.value
-    })
+  itens.push({
+    desc: descricao.value,
+    //converte o número para absoluto
+    val: Math.abs(valor.value),
+    tipo: tipo.value,
+  });
 
-    setItens() 
-    carregarItens()
+  setItens();
+  carregarItens();
 
-    descricao.value = ""
-    valor.value = ""
-
+  descricao.value = "";
+  valor.value = "";
 }
 
 function apagarItens(index) {
-    itens.splice(index, 1);
-    setItens()
-    carregarItens()
+  itens.splice(index, 1);
+  setItens();
+  carregarItens();
 }
 
 function inserirItens(item, index) {
-    let tr = document.createElement("tr")
+  let tr = document.createElement("tr");
 
-
-    tr.innerHTML = `
+  tr.innerHTML = `
         <td>${item.desc}</td>
         <td>${item.val}</td>
         <td class="tipo-coluna">${
-            item.tipo === "Entrada"
-            ? '<i class="fa-solid fa-angle-up" style="color: #33a324;"></i>'
-            : '<i class="fa-solid fa-angle-down" style="color: #af1818;"></i>'
+          item.tipo === "Entrada"
+          ? '<i class="fa-solid fa-angle-up" style="color: #33a324;"></i>'
+          : '<i class="fa-solid fa-angle-down" style="color: #af1818;"></i>'
         }</td>
         <td class="coluna-acao">
             <button onclick="apagarItens(${index})"><i class="fa-solid fa-trash-can" style="color: #29303d;"></i></button>
         </td>
-    `
-    tbody.appendChild(tr)
+    `;
+  tbody.appendChild(tr);
 }
 
-function carregarItens(){
-    itens = getItens()
-    tbody.innerHTML = ""
-    itens.forEach((item, index) => {
-        inserirItens(item, index)
-    })
+function carregarItens() {
+  itens = getItens();
+  tbody.innerHTML = "";
+  itens.forEach((item, index) => {
+    inserirItens(item, index);
+  });
 
-    getTotal()
+  getTotal();
 }
 
 function getTotal() {
-    const valorEntrada = itens
+  const valorEntrada = itens
     .filter((item) => item.tipo === "Entrada")
-    .map(())
+    .map((transacao) => Number(transacao.val));
+
+  const valorSaida = itens
+    .filter((item) => item.tipo === "Saída")
+    .map((transacao) => Number(transacao.val));
+
+  const EntradaTotal = valorEntrada
+    .reduce((acumulado, atual) => acumulado + atual, 0)
+    .toFixed(2);
+
+  const SaidaTotal = Math.abs(
+    valorSaida.reduce((acumulado, atual) => acumulado + atual, 0)
+  ).toFixed(2);
+
+  const valorTotal = (EntradaTotal - SaidaTotal).toFixed(2);
+
+  entrada.innerHTML = EntradaTotal;
+  saida.innerHTML = SaidaTotal
+  total.innerHTML = valorTotal
 }
+
+const getItens = () => JSON.parse(localStorage.getItem("db_itens")) ?? []
+const setItens = () => 
+    localStorage.setItem("db_itens", JSON.stringify(itens)) 
+carregarItens()    
